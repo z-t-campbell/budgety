9a514439773043c2f8bb24637374e5f5383083b3
Zoe Campbell <z.t.campbell1@gmail.com>
2020-02-22 17:44:53 +0000

    coded and tested creating and viewing organisations

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,2 +1,23 @@
 class OrganisationsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create, :show]
+  def new
+    @organisation = Organisation.new
+  end
+
+  def create
+    @organisation = Organisation.new(organisation_params)
+    @organisation.user = current_user
+    @organisation.save
+    redirect_to organisation_path(@organisation)
+  end
+
+  def show
+    @organisation = Organisation.find(params[:id])
+  end
+
+  private
+
+  def organisation_params
+    params.require(:organisation).permit(:name, :location)
+  end
 end
