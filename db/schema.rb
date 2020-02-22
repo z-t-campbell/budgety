6c44d5ca8a664d2453e3edf5e8ada88ffe70ee8d
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2020-02-23 06:12:02 +1300

    created experience controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_155044) do
+ActiveRecord::Schema.define(version: 2020_02_22_155522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,23 @@ ActiveRecord::Schema.define(version: 2020_02_22_155044) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "experiences", force: :cascade do |t|
+    t.string "name"
+    t.string "category"
+    t.text "description"
+    t.string "location"
+    t.date "date"
+    t.time "start_time"
+    t.time "end_time"
+    t.string "venue_name"
+    t.bigint "user_id"
+    t.bigint "organisation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["organisation_id"], name: "index_experiences_on_organisation_id"
+    t.index ["user_id"], name: "index_experiences_on_user_id"
+  end
+
   create_table "organisations", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -58,5 +75,7 @@ ActiveRecord::Schema.define(version: 2020_02_22_155044) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "experiences", "organisations"
+  add_foreign_key "experiences", "users"
   add_foreign_key "organisations", "users"
 end
