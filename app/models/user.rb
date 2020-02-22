a07b26f012687d5f5bb204521c5423b219f1e804
Zoe Campbell <z.t.campbell1@gmail.com>
2020-02-22 17:06:04 +0000

    added routes and navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :experiences
   has_many :organisations
   devise :database_authenticatable, :registerable,
