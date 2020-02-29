63d2c24cab93fe158813dea88f4c4f93477bda45
Donna Cadiente <djanecad@hotmail.com>
2020-02-29 16:41:01 +0000

    fixed user signups, photos and navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :experiences
   has_many :organisations
   has_many :orders
+  has_one_attached :photo
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
