5de4c1760157affa1f3cd72efb01a3a46adc7725
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2020-03-04 07:56:07 +1300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :experiences, only: [:index, :show]
 
   resources :experiences, only: [:show] do
-    resources :orders, only: [:show, :create]
+    resources :orders, only: [:index, :show, :create]
   end
 
   resources :orders, only: [:show, :create] do
