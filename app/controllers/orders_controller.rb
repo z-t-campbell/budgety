26de68545073f087fe8964b3200bbebcb1b56fbd
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2020-03-08 05:02:36 +1300

    done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,14 +5,14 @@ end
 
   def create
   experience = Experience.find(params[:experience_id])
-  order  = Order.create!(experience: experience, amount: experience.price, state: 'pending', user: current_user)
+  order  = Order.create!(experience: experience, amount: params["quantity"].to_i * experience.price, state: 'Booking Confirmed', user: current_user)
 
   session = Stripe::Checkout::Session.create(
     payment_method_types: ['card'],
     line_items: [{
       name: experience.name,
-      images: [experience.photos],
-      amount: experience.price_cents,
+      images: ["https://res.cloudinary.com/dmwa29qvx/image/upload/v1583443299/#{experience.photos.first.key}.jpg"],
+      amount: params["quantity"].to_i * experience.price_cents,
       currency: 'gbp',
       quantity: 1
     }],
