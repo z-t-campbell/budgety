d56772a1f0f07fe7885a3e128ed59bf2305e7828
Zoe Campbell <z.t.campbell1@gmail.com>
2020-03-08 13:49:40 +0000

    debugged orders view to fix conditions to show orders and reviews and fixed the navbar

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,70 +1,62 @@
 class ExperiencesController < ApplicationController
-skip_before_action :authenticate_user!, only: [:new, :create, :show ,:index]
+  skip_before_action :authenticate_user!, only: [:new, :create, :show, :index]
+  before_action :set_experience, only: [:show, :edit, :update, :destroy]
 
-def index
-  @experiences = Experience.all
-  @experiences = @experiences.where("location ILIKE ?", "%#{params[:location]}%") if params[:location].present?
-  @location = params[:location]
-  @experiences = @experiences.where("category ILIKE ?", "%#{params[:category]}%") if params[:category].present?
-  @category = params[:category]
-  @experiences = @experiences.where("price_cents <= ?", params[:budget].to_f*100) if params["budget"].present?
-  @category = params[:budget]
-  # if params[:location].present? && params[:category].present?
-  #   @experiences = Experience.where("location ILIKE ?", "%#{params[:location]}%")
-  #   @location = params[:location]
-  # elsif
-  #   @experiences = Experience.all
-  # end
-  # if params[:query].present?
-  #   @experiences = Experience.where("location ILIKE ?", "%#{params[:query]}%")
-  # else
-  #   @experiences = Experience.all
-  # end
+  def index
+    @experiences = Experience.all
+    @experiences = @experiences.where("location ILIKE ?", "%#{params[:location]}%") if params[:location].present?
+    @location = params[:location]
+    @experiences = @experiences.where("category ILIKE ?", "%#{params[:category]}%") if params[:category].present?
+    @category = params[:category]
+    @experiences = @experiences.where("price_cents <= ?", params[:budget].to_f*100) if params["budget"].present?
+    @budget = params[:budget]
+    @experiences = @experiences.where("category ILIKE ?", "%#{params[:name]}%") if params[:name].present?
+    @budget = params[:name]
+    @experiences = @experiences.where("category ILIKE ?", "%#{params[:date]}%") if params[:date].present?
+    @budget = params[:date]
+  end
 
+  def show
+    @organisation = @experience.organisation
+  end
 
-end
+  def new
+    @organisation = Organisation.find(params[:organisation_id])
+    @experience = Experience.new
+    @categories = ["Arts, Theatre & Shows", "Comedy", "Food & Drink", "Beauty & Spa", "Health & Fitness", "Music Events", "Culture", "Learning", "Family", "Romantic", "Couples", "Groups", "Gift Ideas"]
+  end
 
-def show
-  @experience = Experience.find(params[:id])
-  @organisation = @experience.organisation
-end
+  def create
+    @organisation = Organisation.find(params[:organisation_id])
+    @experience = Experience.new(experience_params)
+    @experience.organisation = @organisation
+    @experience.save
 
-def new
-  @organisation = Organisation.find(params[:organisation_id])
-  @experience = Experience.new
-  @categories = ["Arts, Theatre & Shows", "Comedy", "Food & Drink", "Beauty & Spa", "Health & Fitness", "Music Events", "Culture", "Learning", "Family", "Romantic", "Couples", "Groups", "Gift Ideas"]
-end
+    redirect_to experience_path(@experience)
+  end
 
-def create
-  @organisation = Organisation.find(params[:organisation_id])
-  @experience = Experience.new(experience_params)
-  @experience.organisation = @organisation
-  @experience.save
+  def edit
+    @organisation = Organisation.find(params[:organisation_id])
+  end
 
-  redirect_to experience_path(@experience)
-end
+  def update
+    @experience.update(experience_params)
+    redirect_to experience_path(@experience)
+  end
 
-def edit
-  @experience = Experience.find(params[:id])
-  @organisation = Organisation.find(params[:organisation_id])
-end
+  def destroy
+    @organisation = @experience.organisation
+    @experience.destroy
+    redirect_to organisation_path(@organisation)
+  end
 
-def update
-  @experience = Experience.find(params[:id])
-  @experience.update(experience_params)
-  redirect_to experience_path(@experience)
-end
+  private
 
-def destroy
-  @experience = Experience.find(params[:id])
-  @organisation = @experience.organisation
-  @experience.destroy
-  redirect_to organisation_path(@organisation)
-end
+  def set_experience
+    @experience = Experience.find(params[:id])
+  end
 
-private
-
-def experience_params
-  params.require(:experience).permit(:name, :category, :description, :location, :date, :start_time, :end_time, :price, :photos)
-end
+  def experience_params
+    params.require(:experience).permit(:name, :category, :description, :location, :date, :start_time, :end_time, :price, :photos)
+  end
 end
